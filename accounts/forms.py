c06a282e2b7feb45b3959ee13ebdc6f00d54f295
jin kwon <jinkwon711@gmail.com>
2016-08-11 19:24:32 +0900

    로그인시 사람확인

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 
-# from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm
 
-# class RegistrationForm(UserCreationForm):
+class RegistrationForm(UserCreationForm):
+    quiz_answer = forms.CharField()
--- accounts/views.py
+++ accounts/views.py
@@ -1,19 +1,41 @@
 from django.shortcuts import render
 from django.conf import settings
-from django.contrib.auth.forms import UserCreationForm
+# from django.contrib.auth.forms import UserCreationForm
+from .forms import RegistrationForm
 from django.shortcuts import redirect, render
+import random
+from django.contrib import messages
+
 
 
 
 def register(request):
+    ran_number=int(random.random()*5)
+    print(ran_number)
+    pic_dir = "/static/img/{}.jpg".format(ran_number)
+    answerdict = { 1:"4073", 2:"4030", 3 :"1232", 4:"1284" , 0:"9725"}
+    answer =answerdict.get(ran_number)
+    print(answer)
+
     if request.method == "POST":
-        form = UserCreationForm(request.POST)
+        form = RegistrationForm(request.POST)
         if form.is_valid():
-            form.save()
+            quiz_answer = form.cleaned_data.get("quiz_answer")
+            if quiz_answer == answer:
+                form.save()
+            else:
+                messages.warning(request,"확인문자를 잘못입력하셨습니다")
+                return redirect(".")
         return redirect('/')
+
     else:
-        form = UserCreationForm()
-    return render(request, 'accounts/register.html', {"form":form})
+        form = RegistrationForm()
+    context = {
+        "form":form,
+        "pic_dir": pic_dir,
+    }
+
+    return render(request, 'accounts/register.html', context)
 
 
 
